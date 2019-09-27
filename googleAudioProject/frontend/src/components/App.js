499ff45ae6d313bf5af867a421cc0e2163ee1097
Benjamin Chan <bchan1298@gmail.com>
2019-09-27 15:15:38 -0400

    Added SignUp component

--- googleAudioProject/frontend/src/components/App.js
+++ googleAudioProject/frontend/src/components/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import DataProvider from "./DataProvider";
-import Table from "./Table";
+// import DataProvider from "./DataProvider";
+// import Table from "./Table";
+import SignUp from "./SignUp"
 
-const App = () => (
-  <DataProvider endpoint="api/userManagement/"
-                render={data => <Table data={data} />} />
-);
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    // <DataProvider endpoint="api/userManagement/"
+    //             render={data => <Table data={data} />} />
+
+    return (
+      <SignUp></SignUp>
+    )
+  }
+}
+
+
+
+// const App = () => (
+  
+// );
 
 const wrapper = document.getElementById("app");
-
 wrapper ? ReactDOM.render(<App />, wrapper) : null;
