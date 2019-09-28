13829651185e969208d648fbec2072552d3287c4
Benjamin Chan <bchan1298@gmail.com>
2019-09-27 22:21:30 -0400

    Made it so React handles routing and not Django

--- googleAudioProject/frontend/src/index.js
+++ googleAudioProject/frontend/src/index.js
@@ -1,17 +1,18 @@
+import React from 'react';
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import NavigationBar from "./components/NavigationBar"
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 
 const routing = (
-    <Routing>
-        <div>
-            <Route exact path="/" component={App} />
-            <Route path="userManagement/signIn" component={SignIn} />
-            <Route path="userManagement/signUp" component={SignUp} />
-        </div>
-    </Routing>
+    <Router>
+        <NavigationBar />
+        <Route exact path="/" component={App} />
+        <Route path="/signIn" component={SignIn} />
+        <Route path="/signUp" component={SignUp} />
+    </Router>
 )
 
 ReactDOM.render(routing, document.getElementById('app'));
