6cb1ee9f85e63ea139bacf3d9d1e805faea2a74b
Yuncheng Zhu <yczhu1@bu.edu>
2019-10-18 18:46:28 -0400

    Add file management app

---
+++ googleAudioProject/fileManagement/static/js/audio_album.js
@@ -0,0 +1,8 @@
+const para = document.querySelector('p');
+
+para.addEventListener('click', updateName);
+
+function updateName() {
+  let name = prompt('输入一个新的名字：');
+  para.textContent = '玩家1：' + name;
+}
