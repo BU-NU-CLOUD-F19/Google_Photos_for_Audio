1792bf29ca42292ba63d41517d6ad3d15916ed58
Yuncheng Zhu <yczhu1@bu.edu>
2019-10-18 19:02:10 -0400

    delete js sample

--- googleAudioProject/fileManagement/static/js/audio_album.js
+++
@@ -1,8 +0,0 @@
-const para = document.querySelector('p');
-
-para.addEventListener('click', updateName);
-
-function updateName() {
-  let name = prompt('输入一个新的名字：');
-  para.textContent = '玩家1：' + name;
-}
