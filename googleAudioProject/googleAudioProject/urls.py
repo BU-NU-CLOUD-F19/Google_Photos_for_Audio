954d207df2d281cd14c84446d087801d59d4eae3
Yuncheng Zhu <yczhu1@bu.edu>
2019-10-18 18:53:05 -0400

    Add userpage url

--- googleAudioProject/googleAudioProject/urls.py
+++ googleAudioProject/googleAudioProject/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('userManagement.urls')),
     path('', include('frontend.urls')),
+    path('', include('fileManagement.urls')),
 ]
